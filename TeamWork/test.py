b0fd86d46cf45ba6b2db1c93012827b52a1fd1f2
pkasela <p.kasela@campus.unimib.it>
2019-08-13 17:12:05 +0200

    Implemented @momo's GUI
    Co-Authored-By: null <moiraghif@users.noreply.github.com>

--- TeamWork/test.py
+++ TeamWork/test.py
@@ -1,28 +1,11 @@
 import agents
-import GridWorld
 import constants
-from time import sleep
-import os
+import gui
+import GridWorld
 
-MAX_EPOCHS = 100
 
-players = [agents.Agent(constants.PLAYER1_CHAR),
-           agents.Agent(constants.PLAYER2_CHAR),
-           agents.Agent(constants.DRAGON_CHAR)]
-
-world1 = GridWorld.World(players,False)
-players_alive = {p: True for p in players}
-for epoch in range(MAX_EPOCHS):
-    for p in players:
-        if not players_alive[p]:
-            continue
-        reward, done = p.get_action(epoch+1 == MAX_EPOCHS,0)
-
-        os.system('clear')
-        print(world1)
-        sleep(0.03)
-
-        if done:
-            break
-    if done:
-        break
+p = [agents.Agent(constants.PLAYER1_CHAR),
+     agents.Agent(constants.PLAYER2_CHAR),
+     agents.Agent(constants.DRAGON_CHAR)]
+world1 = GridWorld.World(p, False)
+gui.GUI(world1)
